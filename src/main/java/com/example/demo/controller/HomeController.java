0bc0d5c3900b46011a31c9840335ae56c86772e4
davidvon71 <davidvon71@163.com>
2023-12-30 14:48:43 +0800

    initial

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,18 +1,23 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping("")
     public String hello() {
+        logger.info("/");
         return "Hello!";
     }
 
     @GetMapping("/version")
     public String version() {
+        logger.info("/version");
         return "version=v0.1";
     }
 }
